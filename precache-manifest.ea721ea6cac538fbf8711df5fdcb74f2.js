a423da0b5418efdf95caa75151bc4b1bbc336839
Michał Skóra <skorek.michal@gmail.com>
2020-10-10 13:21:48 +0200

    Updates

---
+++ precache-manifest.ea721ea6cac538fbf8711df5fdcb74f2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6c7e9c8867b7d925331aa5e7963eb0",
+    "url": "/portfolio-template/index.html"
+  },
+  {
+    "revision": "da4a24f743823246798a",
+    "url": "/portfolio-template/static/css/2.df902c81.chunk.css"
+  },
+  {
+    "revision": "af51f319b64391f42b2a",
+    "url": "/portfolio-template/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "da4a24f743823246798a",
+    "url": "/portfolio-template/static/js/2.7ca3c726.chunk.js"
+  },
+  {
+    "revision": "7918f9b893310890f0a444ed75c67550",
+    "url": "/portfolio-template/static/js/2.7ca3c726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af51f319b64391f42b2a",
+    "url": "/portfolio-template/static/js/main.b81bdca6.chunk.js"
+  },
+  {
+    "revision": "2914984b463201b0265d",
+    "url": "/portfolio-template/static/js/runtime-main.dce5ca78.js"
+  },
+  {
+    "revision": "6ced16a70de0cd8b614270f30289e8d4",
+    "url": "/portfolio-template/static/media/WOLFGEO.6ced16a7.jpg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/portfolio-template/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/portfolio-template/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/portfolio-template/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/portfolio-template/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/portfolio-template/static/media/primeicons.79432094.woff"
+  }
+]);
